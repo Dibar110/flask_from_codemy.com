3250b94bbaf1da11cd49d9c416c6d3da1fa4d314
DB <dima.doc110@gmail.com>
2024-08-13 12:30:14 +0300

    second commit

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'secretkey'
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/')
+def index():
+    skills = ['Flask', 'Django', 'JavaScript']
+    return render_template('index.html', skills=skills)
+
 @app.route('/<name>')
-def index(name):
-    items = ['Nasta', True, 110]
-    return render_template('index.html', name=name, items=items)
+def user(name):
+    return render_template('user.html', name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -14,3 +26,12 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('404.html'), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def get_name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name_form.html', name=name, form=form)
