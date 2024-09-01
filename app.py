d434cf3521a208ddb763efae69f774d4298cda2b
DB <dima.doc110@gmail.com>
2024-09-01 09:49:28 +0300

    Dashboard Update Profile

--- app.py
+++ app.py
@@ -240,12 +240,13 @@ def update(id):
     name_to_update = Users.query.get_or_404(id)
     if request.method == 'POST':
         name_to_update.name = request.form['name']
+        name_to_update.username = request.form['username']
         name_to_update.email = request.form['email']
         name_to_update.favorite_color = request.form['favorite_color']
         try:
             db.session.commit()
             flash('User Updated Successfully!')
-            return render_template('update.html', form=form, name_to_update=name_to_update, id=id)
+            return render_template('dashboard.html', form=form, name_to_update=name_to_update, id=id)
         except:
             db.session.commit()
             flash('Error!')
